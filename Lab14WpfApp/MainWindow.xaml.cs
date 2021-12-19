cba17992fc6eaec497adda974009003265ace4c5
Professional <Professional@USER-PC>
2021-12-20 00:58:26 +0300

    Добавьте файлы проекта.

---
+++ Lab14WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab14WpfApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Product> products;
+        public MainWindow()
+        {
+            InitializeComponent();
+            products = new List<Product>();
+            products.Add(new Product()
+            {
+                ProductName="Рыба",
+                ProductCost=350,
+                ProductImage="Data/Fish.png",
+                ProductType=ProductTypes.Food
+            });
+            products.Add(new Product()
+            {
+                ProductName = "Хлеб",
+                ProductCost = 50,
+                ProductImage = "Data/Bread.png",
+                ProductType = ProductTypes.Food
+            });
+            products.Add(new Product()
+            {
+                ProductName = "Ананас",
+                ProductCost = 150,
+                ProductImage = "Data/Pineapple.png",
+                ProductType = ProductTypes.Food
+            });
+            products.Add(new Product()
+            {
+                ProductName = "Стиральная машина",
+                ProductCost = 35000,
+                ProductImage = "Data/WashingMachine.png",
+                ProductType = ProductTypes.Appliances
+            });
+            products.Add(new Product()
+            {
+                ProductName = "Микроволновая печь",
+                ProductCost = 5000,
+                ProductImage = "Data/Microwave.png",
+                ProductType = ProductTypes.Appliances
+            });
+            lstBox.ItemsSource = products;
+        }
+    }
+}
---
+++ Lab14WpfApp/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab14WpfApp
+{
+    public enum ProductTypes
+    {
+        Food,
+        Appliances
+    }
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public double ProductCost { get; set; }
+        public string ProductImage { get; set; }
+        public ProductTypes ProductType { get; set; }
+    }
+}
